415d1935543e48086e4261760a8601a594a106ca
Vojtech Balik <balikvo1@fit.cvut.cz>
2021-02-06 11:56:29 +0100

    day 1

---
+++ d1/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+func main() {
+	var ints []int
+
+	for i := 0; ; i++ {
+		var tmp int
+		_, err := fmt.Scanf(`%d`, &tmp)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				panic(err)
+			}
+		}
+		ints = append(ints, tmp)
+	}
+
+	for i := 0; i < len(ints)-2; i++ {
+		for j := i + 1; j < len(ints)-1; j++ {
+			for k := j + 1; k < len(ints); k++ {
+				if ints[i]+ints[j]+ints[k] == 2020 {
+					fmt.Printf("%d\n", ints[i]*ints[j]*ints[k])
+					break
+				}
+			}
+		}
+	}
+}
